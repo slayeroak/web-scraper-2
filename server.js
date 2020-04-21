aad83b6e9acf2c1644ae459453e3f6cffd0531b2
slayeroak <roakley1@villanova.edu>
2020-04-21 19:28:10 -0400

    routes folder, added morgan package, setup stuff

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
-const express = require("express");
-const mongoose = require("mongoose");
-var logger = require("morgan");
-const exphbs = require("express-handlebars");
-const PORT = process.env.PORT || 8080;
+// dependencies
+const express = require('express');
+const bParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const eHandle = require('express-handlebars');
+
+let PORT = process.env.PORT || 3000;
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/news_scraper';
+
+// initialize express
 const app = express();
 
-//middleware
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
+// use morgan logger for logging requests
+app.use(logger('dev'));
+// use body-parser for handling form submissions
+app.use(bParser.urlencoded({extended: true}));
+// set static directory
+app.use(express.static('public'));
+// Set Handlebars as the default templating engine
+app.engine('handlebars', eHandle({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-//set up handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// database configuration
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {userMongoClient: true});
 
-// mongo connect
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/npr_scraper";
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
+// check connection status
+let db = mongoose.connection;
+db.on('error', (error)=>{
+    console.log(`Connection error ${error}`);
 });
 
 
-
-app.listen(PORT, function () {
-  console.log(
-    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-    PORT,
-    PORT
-  );
+// start server
+app.listen(PORT, ()=>{
+    console.log(`App running on port ${PORT}`);
 });
